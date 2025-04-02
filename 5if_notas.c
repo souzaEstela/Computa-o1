bdc27710b97504ec0d4d934e245338bc4ad00e45
estela_S <101224437+siri-delta@users.noreply.github.com>
2025-04-02 10:33:47 -0300

    Create 5if_notas.c

---
+++ 5if_notas.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    float n1, n2, n3, media;
+
+    printf("Digite as tres notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    media = ((n1+n2+n3)/3);
+    printf("%f\n", media);
+
+    if (media >= 8){
+        printf("Aprovado com Distincao");
+    }
+    else if (media >= 6){
+        printf("Aprovado Direto");
+    }
+
+    else if (media >= 4){
+        printf("Vai para Final" );
+    }
+
+    else {
+        printf("Reprovado Direto ");
+    }
+
+    return 0;
+}
