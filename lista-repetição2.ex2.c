aaeb707eb7a4f0b3791d89371fe8e994fb82f4a7
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-04-16 10:27:44 -0300

    Update lista-repetição2.ex2.c

--- lista-repetição2.ex2.c
+++ lista-repetição2.ex2.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 int main ()
 {
-    int valor, i = 0 , numeros = 0;
-    float media;
-
-    valor = 0;
+    int valor = 0 , i = 0;
+    float media, numeros = 0;
 
             while(valor != -1000)
     {
         printf("Digite um numero, se queiser parar digite -1000: ");
         scanf("%d", &valor);
-        numeros =+ valor;
+        numeros += valor;
         if(valor % 2 == 0)
         {
             i++;
