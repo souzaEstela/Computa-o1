30dd00a74bc3daf04b7c6aae6466188e0f608820
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-04-23 10:01:29 -0300

    Add files via upload

---
+++ provarec1.c
@@ -0,0 +1,23 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+
+    int distancia, tempomin, tempos, min, seg;
+
+    printf("Qual é a distância, em km? ");
+    scanf ("%d", &distancia);
+    printf("Em quantos minutos você percorreu? ");
+    scanf ("%d", &tempomin);
+    printf("Em quantos segundos você percorreu? ");
+    scanf ("%d", &tempos);
+
+    seg = (tempomin * 60) + tempos;
+    seg /= distancia;
+
+    min = seg / 60;
+    seg = seg % 60;
+
+    printf("O seu ritmo foi %d:%d", min, seg);
+    return 0;
+}
