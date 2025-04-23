e5b82146c07f1afda9fd865af21de1749eef3100
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-04-23 09:29:52 -0300

    Add files via upload

---
+++ provarec1.c
@@ -0,0 +1,19 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+
+    float altura, largura, perimetrot, perimetro, area;
+
+    printf("Escreva a altura e a largura do quarto, em metros: ");
+    scanf ("%f %f", &altura, &largura);
+
+    perimetrot = (2*altura + 2*largura);
+    perimetro = (2*altura + 2*largura) - 0.8 ;
+    area = altura*largura;
+
+    printf("O perimetro é: %.1f \n", perimetrot);
+    printf("A quantidade de rodapé necessária é: %.1f m²\n", perimetro);
+    printf("A quantidade de piso necessário é: %.1f m²", area);
+    return 0;
+}
