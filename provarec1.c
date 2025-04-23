f8dae5e1d4548b14b217be8e2979776e943e0ae5
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-04-23 08:48:43 -0300

    Add files via upload

---
+++ provarec1.c
@@ -0,0 +1,22 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+
+    int b, i, f, n;
+
+    printf("Escreva Um número inteiro: ");
+    scanf ("%d", &b);
+    printf("Escreva O valor inicial do intervalo: ");
+    scanf ("%d", &i);
+    printf("Escreva O valor final do intervalo: ");
+    scanf ("%d", &f);
+
+    for ( b!= 0; b*i < f; i++)
+    {
+        n = (b*i) + n;
+    }
+
+    printf("A soma dos múltiplos de %d no intervalo é: %d", b, n);
+    return 0;
+}
