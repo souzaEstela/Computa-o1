4735b3de0b0470ea0759ef1a94efe33e194dba05
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-05-21 09:18:53 -0300

    Create matriz-matriztransp.c

---
+++ matriz-matriztransp.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<time.h>
+
+#define DIMENSAO1 5
+#define DIMENSAO2 5
+
+int main()
+{
+    int m[DIMENSAO1][DIMENSAO2];
+    int i,j,k;
+
+    srand(time(NULL));
+    for(i=0; i<DIMENSAO1; i++)
+        for(j=0; j<DIMENSAO2; j++)
+            m[i][j] = rand()%10+1;
+
+    printf("Matriz: \n");
+
+    for(i=0; i<DIMENSAO1; i++)
+    {
+        for(j=0; j<DIMENSAO2; j++)
+        {
+            printf("%2d ",m[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matriz transposta: \n");
+
+    for(i=0; i<DIMENSAO1; i++)
+    {
+        for(j=0; j<DIMENSAO2; j++)
+        {
+            printf("%2d ",m[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
