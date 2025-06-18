c079d713e56fdb83d029f537f3bcc07b6a963e03
estela_S <101224437+souzaEstela@users.noreply.github.com>
2025-06-18 10:01:54 -0300

    Create ler-adicionar-file.c

---
+++ ler-adicionar-file.c
@@ -0,0 +1,60 @@
+- lê o arquivo e imprime seu conteúdo 
+
+#include <stdio.h>
+
+int main()
+{
+    FILE * arquivo;
+    char conteudo[300];
+
+    arquivo = fopen("teste1.txt", "r");
+
+    if (arquivo == NULL)
+    {
+        printf("Caminho errado!");
+        return 1;
+    }
+
+    while(fgets(conteudo,sizeof(conteudo),arquivo))
+    {
+        printf("%s\n", conteudo);
+    }
+
+    int resultado = fclose(arquivo);
+
+    if (resultado == EOF)
+    {
+        printf("Deu problema!");
+        return 1;
+    }
+  ------------------------------------------------------ outro arquivo -----------------------------------------
+- adiciona uma nova linha
+    #include <stdio.h>
+
+int main()
+{
+    FILE *arquivo;
+    char nova[52];
+
+    arquivo = fopen("teste1.txt", "a");
+
+    if (arquivo == NULL)
+    {
+        printf("Caminho errado");
+        return 1;
+    }
+
+    printf("Qual o novo endereco: ");
+    fgets(nova,sizeof(nova),stdin);
+
+    fputs(nova,arquivo);
+
+    fclose(arquivo);
+    printf("Adicionado");
+
+    return 0;
+}
+
+    fclose(arquivo);
+    return 0;
+}
